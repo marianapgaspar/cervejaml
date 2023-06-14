548aa9c48ec3bc5f30eaf218fac1cec090d9f56c
marianapgaspar <66643112+marianapgaspar@users.noreply.github.com>
2023-06-14 00:40:49 +0000

    file change

--- maquina.py
+++ maquina.py
@@ -3,7 +3,7 @@ from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 import pickle as pickle
 
-nomeArquivo = 'modeloFinalizado.sav'
+nomeArquivo = './modeloFinalizado.sav'
 
 
 def criaModeloDoZero():
