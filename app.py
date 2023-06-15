a14cfa69e1a1a31c2404d2467727fd1c252b01cb
marianapgaspar <66643112+marianapgaspar@users.noreply.github.com>
2023-06-15 01:02:56 +0000

    wsgi

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def verificaPredicao():
   return '{:.2f}'.format(float(resultadoPredicao))
 
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+import sys
+
+sys.path.insert(0, "/var/www/cervejaml")
+
+from app import app as application
